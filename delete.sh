5d32a12e915cc1c32d223fd7c23ef2780c4d5260
Lagadsz <lobo-mf@hotmail.com>
2018-08-30 16:53:53 -0300

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -3,6 +3,7 @@ echo ""
 echo "Input the alias of the node that you want to delete"
 read ALIAS
 transcendence-cli -datadir=/root/.transcendence_$ALIAS stop
+sleep 5
 rm /root/.transcendence_$ALIAS -r
 sed -i '/$ALIAS/d' .bashrc
 sleep 5
