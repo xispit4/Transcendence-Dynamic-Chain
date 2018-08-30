71024d21952bbc469c92f2e5db5c79fdf9049282
Lagadsz <lobo-mf@hotmail.com>
2018-08-30 16:14:37 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -24,7 +24,7 @@ fi
   echo "stop program = \"/root/bin/transcendenced_${ALIAS}.sh stop\"" >> /etc/monit/monitrc
   transcendence-cli -datadir=/root/.transcendence_$ALIAS stop
   monit reload
-  sleep 3
+  sleep 1
   monit
   sleep 1
   /root/bin/transcendenced_${ALIAS}.sh
