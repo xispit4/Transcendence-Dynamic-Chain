c0a512649ac403ff15dfdfa1035e679cc06c5e25
Lagadsz <lobo-mf@hotmail.com>
2018-09-01 17:56:54 -0300

    Update setuplobo.sh

--- setuplobo.sh
+++ setuplobo.sh
@@ -142,6 +142,8 @@ while [  $COUNTER -lt $MNCOUNT ]; do
   echo "masternode=1" >> transcendence.conf_TEMP
   echo "dbcache=50" >> transcendence.conf_TEMP
   echo "banscore=10" >> transcendence.conf_TEMP
+  echo "maxorphantx=10" >> transcendence.conf_TEMP
+  echo "maxmempool=100" >> transcendence.conf_TEMP
   echo "" >> transcendence.conf_TEMP
 
   echo "" >> transcendence.conf_TEMP
@@ -242,6 +244,8 @@ let COUNTER=COUNTER+IP6COUNT
   echo "masternode=1" >> transcendence.conf_TEMP
   echo "dbcache=50" >> transcendence.conf_TEMP
   echo "banscore=10" >> transcendence.conf_TEMP
+  echo "maxorphantx=10" >> transcendence.conf_TEMP
+  echo "maxmempool=100" >> transcendence.conf_TEMP
   echo "" >> transcendence.conf_TEMP
 
   echo "" >> transcendence.conf_TEMP
