2275597d4b4d9d0d5ca6e9bb65d0243935ef2ea9
Lagadsz <lobo-mf@hotmail.com>
2018-09-01 15:49:48 -0300

    Update betasetup.sh

--- betasetup.sh
+++ betasetup.sh
@@ -116,11 +116,11 @@ while [  $COUNTER -lt $MNCOUNT ]; do
   echo ""
   echo "Enter alias for new node"
   read ALIAS
-  if [ -d .transcendence_* ]
+  if [ -d ".transcendence_*" ]
 then
 cp .transcendence_*/* .transcendence_$ALIAS -r
 fi
-  if [ ! -d .transcendence_* ]
+  if [ ! -d ".transcendence_*" ]
 then
 unzip DynamicChain.zip -d ~/.transcendence_$ALIAS
 fi
