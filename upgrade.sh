4a4a093700a17363f6208d7201f1deed0638db1e
Lagadsz <lobo-mf@hotmail.com>
2018-09-04 16:32:44 -0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -3,38 +3,47 @@ perl -i -ne 'print if ! $a{$_}++' /etc/monit/monitrc
 echo "Enter the alias of the node you want to upgrade"
 read ALIAS
 echo ""
-echo "Do you already have a upgraded node in this server? [y/n]"
-read UP
-if [ $UP = "n" ]
-then
-	apt-get install monit=1:5.16-2 -y
-	wget https://raw.githubusercontent.com/Lagadsz/Transcendence-Dynamic-Chain/master/monitrc
-	rm /etc/monit/monitrc
-	cp -a monitrc /etc/monit/monitrc
-	chmod 700 /etc/monit/monitrc
-fi
   ## Config Alias
   echo "alias ${ALIAS}_status=\"transcendence-cli -datadir=/root/.transcendence_$ALIAS masternode status\"" >> .bashrc
-  echo "alias ${ALIAS}_stop=\"transcendence-cli -datadir=/root/.transcendence_$ALIAS stop && monit stop transcendenced${ALIAS} && rm ~/.transcendence_${ALIAS}/transcendenced${ALIAS}.pid\"" >> .bashrc
-  echo "alias ${ALIAS}_start=\"/root/bin/transcendenced_${ALIAS}.sh && sleep 1 && mv ~/.transcendence_${ALIAS}/transcendenced.pid ~/.transcendence_${ALIAS}/transcendenced${ALIAS}.pid && monit start transcendenced${ALIAS}\""  >> .bashrc
+  echo "alias ${ALIAS}_stop=\"transcendence-cli -datadir=/root/.transcendence_$ALIAS stop && systemctl stop transcendenced$ALIAS\"" >> .bashrc
+  echo "alias ${ALIAS}_start=\"/root/bin/transcendenced_${ALIAS}.sh && systemctl start transcendenced$ALIAS\""  >> .bashrc
   echo "alias ${ALIAS}_config=\"nano /root/.transcendence_${ALIAS}/transcendence.conf\""  >> .bashrc
   echo "alias ${ALIAS}_getinfo=\"transcendence-cli -datadir=/root/.transcendence_$ALIAS getinfo\"" >> .bashrc
-  ## Config Monit
-  echo "check process transcendenced${ALIAS} with pidfile /root/.transcendence_${ALIAS}/transcendenced${ALIAS}.pid" >> /etc/monit/monitrc
-  echo "start program = \"/root/bin/transcendenced_${ALIAS}.sh\" with timeout 60 seconds" >> /etc/monit/monitrc
-  echo "stop program = \"/root/bin/transcendenced_${ALIAS}.sh stop\"" >> /etc/monit/monitrc
-  transcendence-cli -datadir=/root/.transcendence_$ALIAS stop
+  ## Config systemd
+  function configure_systemd() {
+  cat << EOF > /etc/systemd/system/transcendenced$ALIAS.service
+[Unit]
+Description=transcendenced$ALIAS service
+After=network.target
+
+[Service]
+User=root
+Group=root
+
+Type=forking
+#PIDFile=/root/.transcendence_$ALIAS/transcendenced.pid
+
+ExecStart=/root/bin/transcendenced_$ALIAS.sh
+ExecStop=-/root/bin/transcendence-cli_$ALIAS.sh stop
+
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  systemctl daemon-reload
+  sleep 3
+  systemctl start transcendenced$ALIAS.service
+}
   perl -i -ne 'print if ! $a{$_}++' /etc/monit/monitrc
-  sleep 1
-  monit reload
-  sleep 1
-  /root/bin/transcendenced_${ALIAS}.sh
-  sleep 4
-  mv ~/.transcendence_${ALIAS}/transcendenced.pid ~/.transcendence_${ALIAS}/transcendenced${ALIAS}.pid 
-  monit 
-  sleep 2
-  monit start transcendenced${ALIAS}
-  echo ""
-  echo "Reboot is recommended after upgrading all your nodes, tho not necessary"
+  perl -i -ne 'print if ! $a{$_}++' .bashrc
+  configure_systemd
+  sleep 5
   exec bash
   exit
