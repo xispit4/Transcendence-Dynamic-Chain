90fbc525525819593b0e88d0b8b6597b5e5f68f3
Lagadsz <lobo-mf@hotmail.com>
2018-10-30 18:04:12 -0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,7 +7,7 @@ echo "Enter number of threads to compile (~1.5gb ram usage per thread)"
 read thr
 sudo add-apt-repository universe -y
 apt-get update
-apt-get install -y git zip unzip build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev devscripts libdb++-dev libsodium-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libminiupnpc-dev qt5-default
+apt-get install -y git zip software-properties-common unzip build-essential libtool autotools-dev autoconf pkg-config libssl-dev libcrypto++-dev libevent-dev libminiupnpc-dev libgmp-dev libboost-all-dev devscripts libdb++-dev libsodium-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libcrypto++-dev libminiupnpc-dev qt5-default
 add-apt-repository ppa:bitcoin/bitcoin -y
 apt-get update
 apt-get install libdb4.8-dev libdb4.8++-dev gcc-5 g++-5 -y --auto-remove
