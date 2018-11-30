f08f23ac3cc8704ba4438078c9cbe66898f68c04
Lagadsz <lobo-mf@hotmail.com>
2018-11-30 21:04:54 -0200

    Update lobohub.sh

--- lobohub.sh
+++ lobohub.sh
@@ -9,10 +9,6 @@ face="$(lshw -C network | grep "logical name:" | sed -e 's/logical name:/logical
   then
   mkdir ~/bin
   fi
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
 if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}$0 must be run as root.${NC}"
    exit 1
